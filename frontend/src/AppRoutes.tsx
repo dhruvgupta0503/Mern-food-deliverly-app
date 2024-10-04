caf574dbf2d8a19ee45c563e3216771977d70cd8
Dhruv Gupta <gdhruv579@gmail.com>
2024-10-04 15:27:47 +0530

    hero page

---
+++ frontend/src/AppRoutes.tsx
@@ -0,0 +1,13 @@
+import { Routes,Route, Navigate } from "react-router-dom"
+import Layout from "./layouts/layout";
+
+const AppRoutes=()=>{
+    return(
+        <Routes>
+            <Route path="/" element={<Layout>Home Page</Layout>} />
+            <Route path="/user-profile" element={<span>user profile</span>} />
+            <Route path="*" element={<Navigate to ="/"/>} />
+        </Routes>
+    );
+};
+export default AppRoutes;
